e4a802a54bfb8ea9671560f6cfb3d4fcb4f393e6
FruHafizd <muhammadhafidz12333@gmail.com>
2026-04-01 18:17:23 +0800

    Deleted Feature Reccuring Transactions

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Livewire\Budgets\BudgetIndex;
 use \App\Livewire\Home\Index as Home;
-use App\Livewire\Transactions\Recurring\Index as RecurringIndex;
 use App\Livewire\Transactions\Index as Transaction;
 use Illuminate\Support\Facades\Route;
 
@@ -10,9 +9,7 @@ Route::redirect('/', '/login');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/home', Home::class)->name('home');
     Route::get('/transaction', Transaction::class)->name('transaction.index');
-    // Route::get('/recurring-transactions', RecurringIndex::class)->name('recurring-transactions'); //disable 
     Route::get('/budget', BudgetIndex::class)->name('budget.index');
-
 
     Route::get('/export-excel', function (\Illuminate\Http\Request $request) {
 
